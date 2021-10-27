119280096b4d3836562d4780bad9952e20fe4c0d
TranceDream <xgxx111@outlook.com>
2021-10-27 19:49:39 +0800

    [feat] 添加学生信息网络请求

--- components/student/info.tsx
+++ components/student/info.tsx
@@ -1,7 +1,20 @@
-import { Flex, Text, Heading, SimpleGrid } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Text, Heading, SimpleGrid, Skeleton } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 
 export default function Info() {
+	const [info, setInfo] = useState(undefined)
+	useEffect(() => {
+		fetch(
+			'http://192.168.31.230:3000/students/personal?student_id=301906060104'
+		)
+			.then((res) => {
+				return res.json()
+			})
+			.then((v) => {
+				setInfo(v)
+			})
+	}, [])
+	console.log(info)
 	return (
 		<Flex w='100%' bg='white' p={12} flexDir='column' borderRadius={8}>
 			<Heading size='xl'>个人资料</Heading>
@@ -10,43 +23,67 @@ export default function Info() {
 					<Text fontSize='lg' fontWeight='bold'>
 						姓名
 					</Text>
-					<Text fontSize='md'>李德新</Text>
+					{info ? (
+						<Text fontSize='md'>{info.msg[0].student_name}</Text>
+					) : (
+						<Skeleton w='3rem' h='1rem' />
+					)}
 				</Flex>
 				<Flex flexDir='column'>
 					<Text fontSize='lg' fontWeight='bold'>
 						学号
 					</Text>
-					<Text fontSize='md'>3019244351</Text>
+					{info ? (
+						<Text fontSize='md'>{info.msg[0].student_id}</Text>
+					) : (
+						<Skeleton w='9rem' h='1rem' />
+					)}
 				</Flex>
 				<Flex flexDir='column'>
 					<Text fontSize='lg' fontWeight='bold'>
 						性别
 					</Text>
-					<Text fontSize='md'>男</Text>
+					{info ? (
+						<Text fontSize='md'>{info.msg[0].gender}</Text>
+					) : (
+						<Skeleton w='1rem' h='1rem' />
+					)}
 				</Flex>
 				<Flex flexDir='column'>
 					<Text fontSize='lg' fontWeight='bold'>
 						年龄
 					</Text>
-					<Text fontSize='md'>24</Text>
+					{info ? (
+						<Text fontSize='md'>{info.msg[0].age}</Text>
+					) : (
+						<Skeleton w='1rem' h='1rem' />
+					)}
 				</Flex>
 				<Flex flexDir='column'>
 					<Text fontSize='lg' fontWeight='bold'>
 						生源所在地
 					</Text>
-					<Text fontSize='md'>下北泽</Text>
+					{info ? (
+						<Text fontSize='md'>{info.msg[0].home_address}</Text>
+					) : (
+						<Skeleton w='6rem' h='1rem' />
+					)}
 				</Flex>
 				<Flex flexDir='column'>
 					<Text fontSize='lg' fontWeight='bold'>
 						已修学分总数
 					</Text>
-					<Text fontSize='md' fontWeight='black'>114514</Text>
+					{info ? (
+						<Text fontSize='md'>{info.msg[0].credits_get}</Text>
+					) : (
+						<Skeleton w='2rem' h='1rem' />
+					)}
 				</Flex>
 				<Flex flexDir='column'>
 					<Text fontSize='lg' fontWeight='bold'>
 						班级编号
 					</Text>
-					<Text fontSize='md'>1919</Text>
+					{info ? <Text fontSize='md'>{info.msg[0].class_id}</Text> : <Skeleton w='3rem' h='1rem' />}
 				</Flex>
 			</SimpleGrid>
 		</Flex>
